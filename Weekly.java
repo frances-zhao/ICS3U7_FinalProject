0d8575fd7660d2903cf2f4a3af5fd3539c149b23
Frances Zhao <84583731+frances-zhao@users.noreply.github.com>
2022-01-11 14:47:50 -0500

    Update Weekly.java

--- Weekly.java
+++ Weekly.java
@@ -1,8 +1,16 @@
 import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 import javax.swing.*;
 
-public class Weekly extends JFrame {
+public class Weekly extends JFrame implements ActionListener{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	JFrame frame = new JFrame("Weekly Calendar");
+	private JButton backbtn;
+	Font newnewfont;
 
 	public static void main(String[] args) {
 		try {
@@ -14,6 +22,12 @@ public class Weekly extends JFrame {
 
 
 	public Weekly() {
+		
+		try {
+			newnewfont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(20f);
+		} catch (IOException | FontFormatException e){
+			
+		}
 		final int HEIGHT = 800;
 		final int WIDTH = 1400;
 		frame.setSize(WIDTH,HEIGHT);
@@ -23,5 +37,27 @@ public class Weekly extends JFrame {
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setVisible(true);
+		
+		backbtn = new JButton("Back");
+		backbtn.setFont(newnewfont);
+		backbtn.setBounds(1213, 117, 135, 77);
+		backbtn.addActionListener(this);
+		
+		frame.add(backbtn);
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == backbtn) {
+			try {
+				new Mainscreen();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			frame.dispose();
+		}
 	}
 }
