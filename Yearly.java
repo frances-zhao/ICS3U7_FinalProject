32dfd2661669b266f3bd56a4ac7b05f4998ee951
Frances Zhao <84583731+frances-zhao@users.noreply.github.com>
2022-01-11 14:48:41 -0500

    Update Yearly.java

--- Yearly.java
+++ Yearly.java
@@ -1,8 +1,15 @@
 import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 import javax.swing.*;
 
-public class Yearly extends JFrame {
+public class Yearly extends JFrame implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
 	JFrame frame = new JFrame("Yearly Calendar");
+	private JButton backbtn;
+	Font newnewfont;
+
 
 	public static void main(String[] args) {
 		try {
@@ -14,6 +21,12 @@ public class Yearly extends JFrame {
 
 
 	public Yearly() {
+		
+		try {
+			newnewfont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(20f);
+		} catch (IOException | FontFormatException e){
+			
+		}
 		final int HEIGHT = 800;
 		final int WIDTH = 1400;
 		frame.setSize(WIDTH,HEIGHT);
@@ -23,5 +36,26 @@ public class Yearly extends JFrame {
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setVisible(true);
+		
+		backbtn = new JButton("Back");
+		backbtn.setFont(newnewfont);
+		backbtn.setBounds(1213, 117, 135, 77);
+		backbtn.addActionListener(this);
+		
+		frame.add(backbtn);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == backbtn) {
+			try {
+				new Mainscreen();
+				frame.dispose();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
 	}
 }
