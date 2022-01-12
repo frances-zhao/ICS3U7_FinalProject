404ac1d8419d5f86313920123c03edb0a2c6d523
Frances Zhao <84583731+frances-zhao@users.noreply.github.com>
2022-01-12 14:52:21 -0500

    Update Mainscreen.java

--- Mainscreen.java
+++ Mainscreen.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 public class Mainscreen extends JFrame implements ActionListener{
 
@@ -14,38 +17,51 @@ public class Mainscreen extends JFrame implements ActionListener{
 	private static JLabel scheduleLabel;
 
 	Font newfont;
+	Font newfont1;
+	Font madefont;
 
-	
 	static Color yellow1 = new Color(243, 215, 3); // tackle logo bg yellow color 
 	static Color yellow2 = new Color(130, 113, 53); // buttons color (dark)
 	static Color yellow3 = new Color(255, 229, 96); // lighter yellow 
 	static Icon icon = new ImageIcon("images/logo.png");
-	static Font font1 = new Font("Tahoma", Font.BOLD, 20);
-	static Font font2 = new Font("Tahoma", Font.PLAIN, 15);
-	static Font font3 = new Font(".AppleSystemUIFont", Font.PLAIN, 19);
 	private static JMenuBar menuBar;
 	private static JMenu settings;
+	private static JMenuItem help;
 	private static JMenuItem colourCSTM;
 	private static JMenuItem switchAcc;
 	private static JMenuItem logout;
+	private JLabel timeLabel;
 
+	int hour, second, minute;
+	int day, month, year;
 
-	// declaring minor decorative details 
+	String time;
+	Calendar calendar;
+	SimpleDateFormat formatTime;
 
 
 	Mainscreen() throws IOException{
-		
+
 		try {
 			newfont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(13f);
+			newfont1 =  Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(40f);
+			madefont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/wake.otf")).deriveFont(40f);
+
 		} catch (IOException | FontFormatException e){
-			
+
 		}
-		
+
 		Main loginpage = new Main();
-	    String userDisplay = loginpage.getUser();
-		
-		//Main loginpage = new Main();
-		//String userDisplay = loginpage.getUser();
+		String userDisplay = loginpage.getUser();
+
+		formatTime = new SimpleDateFormat("hh : mm: ss a");
+
+		timeLabel = new JLabel();
+		timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		timeLabel.setFont(newfont1);
+		timeLabel.setBounds(448, 117, 516, 102);
+
+
 
 		final int HEIGHT = 800;
 		final int WIDTH = 1400;
@@ -55,31 +71,37 @@ public class Mainscreen extends JFrame implements ActionListener{
 		frame.getContentPane().setBackground(yellow1);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
-		
+
 		weekly = new JButton("Weekly");
-		weekly.setBounds(919, 117, 135, 77);
+		weekly.setBounds(965, 46, 135, 77);
 		weekly.setFont(newfont);
 
 		monthly = new JButton("Monthly");
-		monthly.setBounds(1066, 117, 135, 77);
+		monthly.setBounds(1112, 46, 135, 77);
 		monthly.setFont(newfont);
 
 		yearly = new JButton("Yearly");
-		yearly.setBounds(1213, 117, 135, 77);
+		yearly.setBounds(1259, 46, 135, 77);
 		yearly.setFont(newfont);
 
 		username = new JLabel(userDisplay); // get a different public variable to store user input
-		username.setBounds(50, 105, 429, 59);
-		username.setFont(new Font(".AppleSystemUIFont", Font.PLAIN, 33));
-		
+		username.setHorizontalAlignment(SwingConstants.CENTER);
+		username.setBounds(385, 46, 633, 59);
+		username.setFont(madefont);
+
 		menuBar = new JMenuBar();
-		menuBar.setBounds(12, 20, 73, 22);
+		menuBar.setBounds(6, 6, 69, 22);
 		menuBar.setFont(newfont);
 		frame.add(menuBar);
 
 		settings = new JMenu("Settings");
-		settings.setFont(newfont);
 		menuBar.add(settings);
+		settings.setFont(newfont);
+
+
+		help = new JMenuItem("Help");
+		help.setFont(newfont);
+		settings.add(help);
 
 		colourCSTM = new JMenuItem("Colour Customization");
 		colourCSTM.setFont(newfont);
@@ -93,14 +115,15 @@ public class Mainscreen extends JFrame implements ActionListener{
 		logout.setFont(newfont);
 		settings.add(logout);
 
-		scheduleLabel = new JLabel("SCHEDULER");
-		scheduleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		scheduleLabel.setBounds(919, 54, 429, 51);
-		scheduleLabel.setFont(newfont);
-
+		help.addActionListener(this);
 		colourCSTM.addActionListener(this);
 		switchAcc.addActionListener(this);
 		logout.addActionListener(this);
+
+		scheduleLabel = new JLabel("SCHEDULER");
+		scheduleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		scheduleLabel.setBounds(965, 6, 429, 51);
+		scheduleLabel.setFont(newfont);
 		weekly.addActionListener(this);
 		monthly.addActionListener(this);
 		yearly.addActionListener(this);
@@ -110,22 +133,28 @@ public class Mainscreen extends JFrame implements ActionListener{
 		frame.add(yearly);
 		frame.add(username);
 		frame.add(scheduleLabel);
+		frame.add(timeLabel);
 
-	
 		frame.setVisible(true);
 
+		setTime();
+
 	}
 
-	static void Screen() throws IOException {	
-		// set basic format of the screen 
-
-		new Mainscreen();
-
-
-		// why qwq
-		// try remove the frame from the frame.add(component) in Mainscreen constructor
-		// and adding a method to make the addition work? 
+	public void setTime() {
+		while (true) {
+			time = formatTime.format(Calendar.getInstance().getTime());
+			timeLabel.setText(time);
+			
+			try {
+				Thread.sleep(1000);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -133,17 +162,14 @@ public class Mainscreen extends JFrame implements ActionListener{
 		if (e.getSource() == weekly) {
 			new Weekly();
 			frame.dispose(); // dispose of current frame
-
 		}
 		if (e.getSource() == monthly) {
 			new Monthly();
 			frame.dispose(); // dispose of current frame
-
 		}
 		if (e.getSource() == yearly) {
 			new Yearly();
 			frame.dispose(); // dispose of current frame
-
 		}
 		if (e.getSource()== logout) {
 			new Logout();
@@ -151,11 +177,13 @@ public class Mainscreen extends JFrame implements ActionListener{
 		if (e.getSource()== switchAcc) {
 			new SwitchAcc();
 		}
-		
+
 		if (e.getSource()== colourCSTM) {
 			new ColourCSTM();
 		}
-		
+		if (e.getSource()== help) {
+			new Help();
+		}
 
 
 	}
@@ -167,4 +195,5 @@ public class Mainscreen extends JFrame implements ActionListener{
 			e.printStackTrace();
 		}
 	}
+
 }
