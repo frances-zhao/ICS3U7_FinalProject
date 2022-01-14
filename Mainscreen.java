47822ef60e7ec00f014f0eced923ac98f38c03f4
Frances Zhao <84583731+frances-zhao@users.noreply.github.com>
2022-01-14 13:34:40 -0500

    Update Mainscreen.java

--- Mainscreen.java
+++ Mainscreen.java
@@ -9,7 +9,7 @@ import java.util.Calendar;
 public class Mainscreen extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
-	
+
 	// declare all components to be used for the main screen 
 	static JFrame frame = new JFrame("Tackle");
 	static JPanel panel; 
@@ -20,11 +20,8 @@ public class Mainscreen extends JFrame implements ActionListener{
 	static Color yellow1 = new Color(255, 208, 37); // tackle logo bg yellow color 
 	static Color yellow2 = new Color(232, 180, 2); // buttons color (dark)
 	static Color yellow3 = new Color(255, 255, 255); // white
-	static Font newfont;
-	static Font newfont1;
-	static Font newfont2;
-	static Font newfont3;
-	static Font madefont;
+	static Font newfont, newfontsmall, newfont1,newfont2, madefont, newfont3 ;
+
 	static Icon icon = new ImageIcon("images/logo.png");
 	private static JMenuBar menuBar;
 	private static JMenu settings;
@@ -32,25 +29,28 @@ public class Mainscreen extends JFrame implements ActionListener{
 	private static JMenuItem colourCSTM;
 	private static JMenuItem switchAcc;
 	private static JMenuItem logout;
-	private JLabel timeLabel;
+	JLabel timeLabel, dayLabel, dateLabel;
 
 	int hour, second, minute;
-	int day, month, year;
 
-	String time;
+	String time, day, date;
 	Calendar calendar;
 	SimpleDateFormat formatTime;
+	SimpleDateFormat dayFormat;
+	SimpleDateFormat dateFormat;
 
 
-	Mainscreen() throws IOException{
+
+
+	public Mainscreen() throws IOException{
 
 		try {
+			newfontsmall = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(15f);
 			newfont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(20f);
 			newfont1 = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(30f);
 			newfont2 =  Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(40f);
 			newfont3 = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(60f);
-			madefont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/wake.otf")).deriveFont(15f);
-
+			madefont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/wake.otf")).deriveFont(20f);
 
 		} catch (IOException | FontFormatException e){
 
@@ -60,14 +60,25 @@ public class Mainscreen extends JFrame implements ActionListener{
 		String userDisplay = loginpage.getUser();
 
 		formatTime = new SimpleDateFormat("hh : mm: ss a");
-
 		timeLabel = new JLabel();
 		timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		timeLabel.setFont(newfont3);
 		timeLabel.setBounds(95, 95, 516, 102);
 
+		dayFormat = new SimpleDateFormat("EEEE");
+		dayLabel = new JLabel();
+		dayLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		dayLabel.setFont(newfont2);		
+		dayLabel.setBounds(43, 131, 622, 141);
 
+		dateFormat = new SimpleDateFormat("MMMMM dd, yyyy");
+		dateLabel = new JLabel();
 
+		dateLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		dateLabel.setFont(newfont2);		
+		dateLabel.setBounds(43, 170, 622, 141);
+
+		
 		final int HEIGHT = 800;
 		final int WIDTH = 1400;
 		frame = new JFrame("Tackle"); // title of application
@@ -83,14 +94,14 @@ public class Mainscreen extends JFrame implements ActionListener{
 		weekly.setFont(newfont);
 		weekly.setForeground(yellow3);
 		weekly.setBorder(BorderFactory.createLineBorder(yellow2));
-		
+
 		monthly = new JButton("Monthly");
 		monthly.setBounds(1066, 117, 135, 77);
 		monthly.setBackground(yellow2);
 		monthly.setFont(newfont);
 		monthly.setForeground(yellow3);
 		monthly.setBorder(BorderFactory.createLineBorder(yellow2));
-		
+
 		yearly = new JButton("Yearly");
 		yearly.setBounds(1213, 117, 135, 77);
 		yearly.setBackground(yellow2);
@@ -99,36 +110,34 @@ public class Mainscreen extends JFrame implements ActionListener{
 		yearly.setBorder(BorderFactory.createLineBorder(yellow2));
 
 
-
 		username = new JLabel(userDisplay); // get a different public variable to store user input
 		username.setHorizontalAlignment(SwingConstants.CENTER);
 		username.setBounds(385, 46, 633, 59);
 		username.setFont(madefont);
 
 		menuBar = new JMenuBar();
-		menuBar.setBounds(6, 6, 69, 22);
+		menuBar.setBounds(12, 12, 80, 22);
 		menuBar.setFont(madefont);
 		frame.add(menuBar);
 
 		settings = new JMenu("Settings");
 		menuBar.add(settings);
-		settings.setFont(madefont);
-
+		settings.setFont(newfontsmall);
 
 		help = new JMenuItem("Help");
-		help.setFont(madefont);
+		help.setFont(newfontsmall);
 		settings.add(help);
 
 		colourCSTM = new JMenuItem("Colour Customization");
-		colourCSTM.setFont(madefont);
+		colourCSTM.setFont(newfontsmall);
 		settings.add(colourCSTM);
 
 		switchAcc = new JMenuItem("Switch Accounts");
-		switchAcc.setFont(madefont);
+		switchAcc.setFont(newfontsmall);
 		settings.add(switchAcc);
 
 		logout = new JMenuItem("Logout");
-		logout.setFont(madefont);
+		logout.setFont(newfontsmall);
 		settings.add(logout);
 
 		help.addActionListener(this);
@@ -150,10 +159,13 @@ public class Mainscreen extends JFrame implements ActionListener{
 		frame.add(username);
 		frame.add(scheduleLabel);
 		frame.add(timeLabel);
-
+		frame.add(dayLabel);
+		frame.add(dateLabel);
+		
 		frame.setVisible(true);
-
-		setTime();
+		
+		
+		//setTime(); does not work with main program
 
 	}
 
@@ -161,6 +173,11 @@ public class Mainscreen extends JFrame implements ActionListener{
 		while (true) {
 			time = formatTime.format(Calendar.getInstance().getTime());
 			timeLabel.setText(time);
+			
+			day = dayFormat.format(Calendar.getInstance().getTime());
+			dayLabel.setText(day);
+			date = dateFormat.format(Calendar.getInstance().getTime());
+			dateLabel.setText(date);
 			
 			try {
 				Thread.sleep(1000);
@@ -170,46 +187,47 @@ public class Mainscreen extends JFrame implements ActionListener{
 		}
 		
 	}
+	
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			if (e.getSource() == weekly) {
+				new Weekly();
+				frame.dispose(); // dispose of current frame
+			}
+			if (e.getSource() == monthly) {
+				new Monthly();
+				frame.dispose(); // dispose of current frame
+			}
+			if (e.getSource() == yearly) {
+				new Yearly();
+				frame.dispose(); // dispose of current frame
+			}
+			if (e.getSource()== logout) {
+				new Logout();
+			}
+			if (e.getSource()== switchAcc) {
+				new SwitchAcc();
+			}
+
+			if (e.getSource()== colourCSTM) {
+				new ColourCSTM();
+			}
+			if (e.getSource()== help) {
+				new Help();
+			}
 
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
+		}
+		public static void main(String[] args) {
+			try {
+				frame.dispose();
+				new Mainscreen();
 
-		if (e.getSource() == weekly) {
-			new Weekly();
-			frame.dispose(); // dispose of current frame
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		if (e.getSource() == monthly) {
-			new Monthly();
-			frame.dispose(); // dispose of current frame
-		}
-		if (e.getSource() == yearly) {
-			new Yearly();
-			frame.dispose(); // dispose of current frame
-		}
-		if (e.getSource()== logout) {
-			new Logout();
-		}
-		if (e.getSource()== switchAcc) {
-			new SwitchAcc();
-		}
-
-		if (e.getSource()== colourCSTM) {
-			new ColourCSTM();
-		}
-		if (e.getSource()== help) {
-			new Help();
-		}
-
 
 	}
-	public static void main(String[] args) {
-		try {
-			new Mainscreen();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-}
