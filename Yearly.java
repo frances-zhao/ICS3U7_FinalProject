12d33744ef49e8a37c062cc70d843fae405221a2
Frances Zhao <frances.zhao@student.tdsb.on.ca>
2022-01-26 15:03:09 -0500

    updating Yearly.java

--- Yearly.java
+++ Yearly.java
@@ -1,52 +1,106 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+// import packages
 import java.awt.event.*;
-import java.io.*;
+import java.text.*;
+import java.util.*;
 import javax.swing.*;
 
-public class Yearly extends JFrame implements ActionListener{
 
+public class Yearly extends YearlySize implements ActionListener{
+
+	// variable declarations
 	private static final long serialVersionUID = 1L;
-	JFrame frame = new JFrame("Yearly Calendar");
-	private JButton backbtn;
-	Font newnewfont;
+	JButton next = new JButton("Next");
+	JButton back = new JButton("Back");
+	JButton home = new JButton("Home");
+	JFrame frame= new JFrame();
 
 
 	public Yearly() {
-		
-		try {
-			newnewfont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(20f);
-		} catch (IOException | FontFormatException e){
-			
-		}
-		final int HEIGHT = 800;
-		final int WIDTH = 1400;
-		frame.setSize(WIDTH,HEIGHT);
+
+		// frame settings
+		frame.getContentPane().setBackground(yellow1);
+		frame.setSize(1400, 800);
+		frame.setLayout(new GridLayout(0, 5, 0, 0)); // grid layout = 5 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setBackground(new Color(243, 215, 3));
-		frame.setLayout(null);
 		frame.setLocationRelativeTo(null);
-		frame.setResizable(false);
+
+		// importing icon for next button and settings
+		Icon nexticon = new ImageIcon("images/nextarrow.png");
+		next = new JButton(nexticon);
+		next.setOpaque(false);
+		next.setContentAreaFilled(false);
+		next.setBorderPainted(false);
+
+		// importing icon for back button and settings
+		Icon backicon = new ImageIcon("images/backarrow.png");
+		back = new JButton(backicon);
+		back.setOpaque(false);
+		back.setContentAreaFilled(false);
+		back.setBorderPainted(false);
+
+		// importing icon for home button and settings
+		Icon homeicon = new ImageIcon("images/logo.png");
+		home = new JButton(homeicon);
+		home.setOpaque(false);
+		home.setContentAreaFilled(false);
+		home.setBorderPainted(false);
+
+		// adding action listener
+		home.addActionListener(this);
+		next.addActionListener(this);
+		back.addActionListener(this);
+
+		// adding in the twelve months (twelve set calendars)
+		for (int i = 0; i < 12; i++) {
+			YearlySize ch = new YearlySize();
+			ch.setDate(new Date(yearint, i, 1));
+			frame.add(ch);
+		}
+		// pack frame: at or above preferred size
+		frame.pack();
+
+		// actual GUI
+		frame.add(back);
+		frame.add(home);
+		frame.add(next);
+
+		// set frame to visible
 		frame.setVisible(true);
-		
-		backbtn = new JButton("Back");
-		backbtn.setFont(newnewfont);
-		backbtn.setBounds(1213, 117, 135, 77);
-		backbtn.addActionListener(this);
-		
-		frame.add(backbtn);
+
 	}
-	
+
+
+	public static void main(String[]args) {
+		new Yearly();
+	}
+
+
+	// if button clicked, using Action Listener continue accordingly
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		if (e.getSource() == backbtn) {
+		if (e.getSource() == next) {
+			yearint++; // move to the next year
+			new Yearly();
+			frame.dispose();
+
+		}
+		if (e.getSource() == back) {
+			yearint--; // move to the previous year
+			new Yearly();
+			frame.dispose();
+
+		}
+		if (e.getSource() == home) {
 			try {
 				new Mainscreen();
-				frame.dispose();
 			} catch (IOException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
+			frame.dispose();
 		}
 	}
 }
---
+++ YearlySize.java
@@ -0,0 +1,70 @@
+// import packages
+import java.awt.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import javax.swing.*;
+
+
+public class YearlySize extends JPanel {
+
+	// variable declarations
+	private static final long serialVersionUID = 1L;
+	static int yearint = 122;
+	// using simpledateformat, declare format of year month and date
+	SimpleDateFormat yearformat = new SimpleDateFormat("yyyy");
+	SimpleDateFormat monthformat = new SimpleDateFormat("MMMM");
+	SimpleDateFormat dayformat = new SimpleDateFormat("d");
+	// font declarations
+	Font newfont, smallfont;
+	// declaring a date object
+	Date date = new Date();
+
+	static Color yellow1 = new Color(243, 215, 3); // tackle logo bg yellow color 
+	static Color yellow2 = new Color(232, 180, 2); // buttons color (dark)
+	static Color yellow3 = new Color(255, 255, 255); // white
+
+	public YearlySize() {
+		// setting size and colour background of tiny calendar
+		setPreferredSize(new Dimension(280, 260));
+		setBackground(yellow1);
+
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public void paintComponent(Graphics g) { // customizing the mini calendars using paintComponent
+		// try/catch importing the font
+		try {
+			newfont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(20f);
+			smallfont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/PPObjectSans-Regular.otf")).deriveFont(12f);
+
+		} catch (IOException | FontFormatException e){
+
+		}
+
+		super.paintComponent(g);
+
+		g.setFont(newfont); // setting font
+		g.drawString(monthformat.format(date), 34, 40); // setting its display in the mini 'panel'
+		g.setColor(Color.black); // setting colour of month
+		g.setColor(Color.black); // setting color of numbers
+		g.drawString(yearformat.format(date), 200, 40); // setting where year date is displayed
+		g.setFont(smallfont); // setting font of numbers
+
+
+		Calendar cal = Calendar.getInstance(); // declaring new calendar object
+		cal.setTime(date); // setting date of calendar
+		cal.set(Calendar.DATE, 1);
+		cal.add(Calendar.DATE, - cal.get(Calendar.DAY_OF_WEEK) + 1);
+		for (int w = 0; w < 5; w++) { // rows of weeks
+			for (int days = 0; days < 7; days++) { // column of days
+				g.drawString(dayformat.format(cal.getTime()), days * 30 + 35 + 4, w * 30 + 85);
+				cal.add(Calendar.DATE, + 1); // adding number to calendar
+			}
+		}
+	}
+
+}
